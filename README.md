37f01261c958549cf9e9069ad2244418c0a71443
_sasha <s.iatsenia@gmail.com>
2017-09-12 13:21:56 -0700

    record videos and show recordings on page

---
+++ README.md
@@ -0,0 +1,11 @@
+# Pi camera interface
+
+Small app that helps capturing video using the Raspberry Pi camera. Written in flask.
+
+## Setup
+
+Run `pip3 install -requirements.txt` to install the python requirements.
+
+Run `sudo apt-get install -y gpac ffmpeg` to install a video encoding utility.
+
+Then run `sudo python3 pi-camera-interface.py`.
--- pi-camera-interface.py
+++ pi-camera-interface.py
@@ -1,15 +1,30 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, send_from_directory
 import subprocess
+import time
+import os
 
-app = Flask(__name__, static_url_path='/static')
+app = Flask(__name__)
 
 @app.route('/', methods=['POST', 'GET'])
 def camera_control():
     if request.method == 'POST':
-        subprocess.call(["raspivid","-o", "test2.mp4"])
+        timestamp = int(time.time())
+        subprocess.call(["raspivid","-o", "recordings/pi-{}.h264".format(timestamp)])
+        subprocess.call(["MP4Box", "-add", "recordings/pi-{}.h264".format(timestamp), "recordings/pi-{}.mp4".format(timestamp)])
+        subprocess.call(["ffmpeg", "-i", "recordings/pi-{}.mp4".format(timestamp), "-vframes", "1", "-o", "thumbs/pi-{}.mp4.jpg".format(timestamp)])
+        subprocess.call(["rm", "recordings/pi-{}.h264".format(timestamp)])
         return render_template('camera-interface.html')
     else:
-        return render_template('camera-interface.html')
+        saved_recordings = os.listdir('recordings')
+        return render_template('camera-interface.html', saved_recordings=saved_recordings)
+
+@app.route('/recordings/<path:filename>')
+def serve_video(filename):
+    return send_from_directory('recordings', filename=filename, mimetype='video/mp4')
+
+@app.route('/thumbs/<path:filename>')
+def serve_thumbnail(filename):
+    return send_from_directory('thumbs', filename=filename, mimetype='img/jpeg')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=80, debug=True)

939cbe1a926c73c2154b039d1fc7f676a1a89b3f
_sasha <s.iatsenia@gmail.com>
2017-09-12 14:23:00 -0700

    improving performance, adding clearer timestamps, more feedback to user

--- pi-camera-interface.py
+++ pi-camera-interface.py
@@ -4,19 +4,25 @@ import time
 import os
 
 app = Flask(__name__)
+os.environ['TZ'] = 'America/Los_Angeles'
+time.tzset()
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/')
 def camera_control():
+    saved_recordings = os.listdir('recordings')
+    return render_template('camera-interface.html', saved_recordings=saved_recordings)
+
+@app.route('/record', methods=['POST', 'GET'])
+def record():
     if request.method == 'POST':
-        timestamp = int(time.time())
+        timestamp = time.strftime('%H-%M-%S_%Y-%m-%d')
         subprocess.call(["raspivid","-o", "recordings/pi-{}.h264".format(timestamp)])
         subprocess.call(["MP4Box", "-add", "recordings/pi-{}.h264".format(timestamp), "recordings/pi-{}.mp4".format(timestamp)])
-        subprocess.call(["ffmpeg", "-i", "recordings/pi-{}.mp4".format(timestamp), "-vframes", "1", "-o", "thumbs/pi-{}.mp4.jpg".format(timestamp)])
+        subprocess.call(["ffmpeg", "-i", "recordings/pi-{}.mp4".format(timestamp), "-vframes", "1", "thumbs/pi-{}.mp4.jpg".format(timestamp)])
         subprocess.call(["rm", "recordings/pi-{}.h264".format(timestamp)])
-        return render_template('camera-interface.html')
+        return render_template('processing.html')
     else:
-        saved_recordings = os.listdir('recordings')
-        return render_template('camera-interface.html', saved_recordings=saved_recordings)
+        return render_template('processing.html')
 
 @app.route('/recordings/<path:filename>')
 def serve_video(filename):
